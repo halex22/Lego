1e987dd1ca5e536301c979551e99c60621046e30
halex22 <halex2209@gmail.com>
2025-06-06 13:20:09 +0200

    work

---
+++ Lego/Logic/LegoUnitWork.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+
+namespace Lego.Logic
+{
+    internal class LegoUnitWork : IUnitOfWork
+    {
+        private readonly DbContext _context;
+        private readonly Dictionary<Type, object> _repositories = new();
+
+        
+        public void BeginTransaction()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Commit()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Rollback()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void SaveChanges()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
