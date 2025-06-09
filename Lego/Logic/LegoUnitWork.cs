52d6908377f2195dfa1660e80e957d58e017ae60
halex22 <halex2209@gmail.com>
2025-06-09 09:04:26 +0200

    render generic get repository for all db sets

--- Lego/Logic/LegoUnitWork.cs
+++ Lego/Logic/LegoUnitWork.cs
@@ -1,18 +1,46 @@
-﻿using System;
+﻿using Lego.model;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Storage;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-using Microsoft.EntityFrameworkCore;
 
 namespace Lego.Logic
 {
     internal class LegoUnitWork : IUnitOfWork
     {
         private readonly DbContext _context;
-        private readonly Dictionary<Type, object> _repositories = new();
+        private readonly Dictionary<Type, object> _repositories = [];
+        private IDbContextTransaction? _transaction;
 
-        
+        public LegoUnitWork(DbContext context)
+        {
+            _context = context ?? throw new ArgumentNullException(nameof(context));
+
+        }
+
+        public IRepository<T> GetRepository<T>() where T : class
+        {
+            var type = typeof(T);
+            if (!_repositories.TryGetValue(type, out var repository))
+            {
+                repository = new LegoRepository<T>(_context);
+                _repositories[type] = repository;
+            }
+
+            return (IRepository<T>)repository;
+        }
+
+        public IEnumerable<T> GetAll<T>() where T : class
+        {
+            return GetRepository<T>().GetAll();
+        }
+
+
+
+
         public void BeginTransaction()
         {
             throw new NotImplementedException();
--- Lego/Program.cs
+++ Lego/Program.cs
@@ -1,19 +1,34 @@
-﻿namespace Lego
+﻿using Lego.Logic;
+using Lego.model;
+
+namespace Lego
 {
     internal class Program
     {
         static void Main(string[] args)
         {
             var context = new LegoContext();
-            var colorRepo = new Lego.Logic.LegoRepository<Lego.model.LegoColor>(context);
-            var colors = colorRepo.GetAll();
+            //var colorRepo = new Lego.Logic.LegoRepository<Lego.model.LegoColor>(context);
+            //var colors = colorRepo.GetAll();
+            //foreach (var color in colors)
+            //{
+            //    Console.WriteLine($"Color ID: {color.Id}, Name: {color.Name}, RGB: {color.Rgb}");
+            //}
+
+            //var themeRepo = new Lego.Logic.LegoRepository<Lego.model.LegoTheme>(context);
+            //var themes = themeRepo.GetAll();
+            //foreach (var theme in themes)
+            //{
+            //    Console.WriteLine($"Theme ID: {theme.Id}, Name: {theme.Name}");
+            //}
+            var uow = new LegoUnitWork(context);
+            var colors = uow.GetRepository<LegoColor>().GetAll();
             foreach (var color in colors)
             {
                 Console.WriteLine($"Color ID: {color.Id}, Name: {color.Name}, RGB: {color.Rgb}");
             }
 
-            var themeRepo = new Lego.Logic.LegoRepository<Lego.model.LegoTheme>(context);
-            var themes = themeRepo.GetAll();
+            var themes = uow.GetRepository<LegoTheme>().GetAll();
             foreach (var theme in themes)
             {
                 Console.WriteLine($"Theme ID: {theme.Id}, Name: {theme.Name}");
